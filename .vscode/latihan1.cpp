54c2bb8eea79d1bde173d31850c012d07c0045b8
defarrel <Defarrel>
2023-03-08 16:05:58 +0700

    first commit

---
+++ .vscode/latihan1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    double Luas, P, L ;
+    cout << "masukan nilai p: ";
+    cin >> P;
+    cout << "masukan nilai l: ";
+    cin >> L;
+    Luas = P*L;
+    cout << "Luasnya: " << Luas;
+}
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+    int main(){
+        int p,l,hasil;
+        p=3;
+        l=10;
+        hasil=p*l;
+        cout << "hasil = " << hasil << endl;
+
+            return 0;
+    }
